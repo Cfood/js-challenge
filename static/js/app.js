913effe2c3e07d91283e8e202a0eb6a15dca484e
CYM <cyloumiller@gmail.com>
2021-06-25 17:18:02 -0500

    rows

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,46 @@
 // from data.js
 var tableData = data;
+// from data.js
 
-// YOUR CODE HERE!
+
+function insertData() {
+
+
+    var tableBody = d3.select("tbody");
+
+    tableBody.html('');
+
+    for (let i = 0; i < tableData.length; i++) {
+
+        var tableRow = tableBody.append("tr")
+        
+        tableRow.append("td").text(tableData[i].datetime);
+        tableRow.append("td").text(tableData[i].city);
+        tableRow.append("td").text(tableData[i].state);
+        tableRow.append("td").text(tableData[i].country);
+        tableRow.append("td").text(tableData[i].shape);
+        tableRow.append("td").text(tableData[i].durationMinutes);
+        tableRow.append("td").text(tableData[i].comments);
+
+        }
+
+
+    }
+
+
+    
+
+
+// }
+
+
+insertData();
+
+
+// create table
+
+// for element in data
+//     new table row
+//     for element in element
+//         create col
+//         insert data element
